0ff8c112a10bfe2770ef3a4ea0bfbd3e3da3b948
flyq <flyq951@gmail.com>
2019-11-22 22:47:19 -0800

    finish the p1170

--- leetcode/p1170/src/main.rs
+++ leetcode/p1170/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = vec!["aabbbabaa".to_string()];
+    let b = vec!["b".to_string(),"aaaba".to_string(),"aaaabba".to_string(),"aa".to_string(),"aabaabab".to_string(),"aabbaaabbb".to_string(),"ababb".to_string(),"bbb".to_string(),"aabbbabb".to_string(),"aab".to_string(),"bbaaababba".to_string(),"baaaaa".to_string()];
+    println!("{:?}", Solution::num_smaller_by_frequency(a, b));
 }
+
 pub struct Solution{}
 
 impl Solution {
     pub fn num_smaller_by_frequency(queries: Vec<String>, words: Vec<String>) -> Vec<i32> {
-        let mut res = vec![0];
+        let mut res = vec![];
         let lenq = queries.len();
         let lenw = words.len();
         let mut q = vec![0;lenq];
@@ -18,22 +23,15 @@ impl Solution {
             w[i] = Solution::fun(&words[i]);
         }
         w.sort();
-        
+              
         for i in 0..lenq {
-            let mut j = lenw/2;
-            let mut max = 0;
-            let mut min = lenw -1;
-            while j > min && j < max {
-                if words[j] < queries[i] {
-                    min = j;
-                    j = (min + max)/2;
-                } else if words[j] == queries[i] {
-                }
-            }
+            let t = q[i];
+            let temp = Solution::binary_search(&w,t);
+            res.push(temp);
         }
-
-        vec![0]
+        res
     }
+    
     pub fn fun(a: &String) -> i32 {
         let char_vec: Vec<char> = a.chars().collect();
         let mut min = 'z';
@@ -48,7 +46,39 @@ impl Solution {
         }
         min_count
     }
+    
     pub fn binary_search(a: &Vec<i32>, t: i32) -> i32 {
-        
+        let mut min = 0;
+        let mut max = a.len() - 1;
+        if a[max] <= t {
+            return 0;
+        } 
+        while min+1 < max {
+            let temp = (min+max)/2;
+            if a[temp] > t {
+                max = temp;
+            } else {
+                min = temp;
+            }
+        }
+        (a.len() - max) as i32
+    }       
 }
 
+
+/*
+执行结果：
+通过
+显示详情
+执行用时 :
+4 ms
+, 在所有 rust 提交中击败了
+100.00%
+的用户
+内存消耗 :
+2.1 MB
+, 在所有 rust 提交中击败了
+100.00%
+的用户
+*/
+    
