f19be47a2bed01961a17c31b33224b1fb7ca0754
flyq <flyq951@gmail.com>
2019-12-03 21:53:57 -0800

    add p0970

---
+++ leetcode/p0970/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+pub struct Solution{}
+
+impl Solution {
+    pub fn powerful_integers(x: i32, y: i32, bound: i32) -> Vec<i32> {
+        let mut v = vec![];
+        let mut temp = 0;
+        while temp <= bound {
+            temp = x.pow(
+    }
+}
