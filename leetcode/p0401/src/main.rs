1ea48c01d351adf0effe353641302121ce80b738
flyq <flyq951@gmail.com>
2019-12-16 01:31:28 -0800

    add p0100 p0401

---
+++ leetcode/p0401/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct Solution{}
+
+impl Solution {
+    pub fn read_binary_watch(num: i32) -> Vec<String> {
+        let mut res: Vec<String> = vec![];
+        if num == 0 {
+            return vec![];
+        } else if num < 5 {
+            for i in 0..num+1 {
+            }
+        }
+        vec![]
+                
+    }
+}
