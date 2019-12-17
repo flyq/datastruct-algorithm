fe6b8a16239fe5387cf33191c82eafa0554e5e40
flyq <flyq951@gmail.com>
2019-12-17 07:15:44 -0800

    add leectode/p0101

---
+++ leetcode/p0101/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("Hello, world!");
+}
+pub struct Solution {}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        
+    }
+}
