7da3e172ce109b2bb4d7db79fabf75d22bec829a
flyq <flyq951@gmail.com>
2020-01-19 07:31:35 -0800

    add A

--- leetcode/p0226/src/main.rs
+++ leetcode/p0226/src/main.rs
@@ -28,6 +28,31 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        
+        match root {
+            Some(node_ref) => {
+                Some(Rc::new(RefCell::new(
+                    TreeNode {
+                        val: node_ref.borrow().val,
+                        left: Solution::invert_tree(node_ref.borrow().right.clone()),
+                        right: Solution::invert_tree(node_ref.borrow().left.clone()),
+                    }
+                )))
+            },
+            None => None,
+        }
     }
 }
+
+
+/*
+执行用时 :
+0 ms
+, 在所有 Rust 提交中击败了
+100.00%
+的用户
+内存消耗 :
+2 MB
+, 在所有 Rust 提交中击败了
+71.43%
+的用户
+*/
