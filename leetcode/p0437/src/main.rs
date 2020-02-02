9e09a778f038bdad833e755c5143c66b07149376
flyq <flyq951@gmail.com>
2020-02-02 07:41:24 -0800

    update p0437

--- leetcode/p0437/src/main.rs
+++ leetcode/p0437/src/main.rs
@@ -2,14 +2,13 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub struct Solution{}
-
+pub struct Solution {}
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
     pub val: i32,
-    pub left: Option<Rc<RefCell<NodeTree>>>,
-    pub right: Option<Rc<RefCell<NodeTree>>>,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
@@ -23,13 +22,69 @@ impl TreeNode {
     }
 }
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 impl Solution {
-    pub fn path_sum_iii(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> i32 {
+    pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> i32 {
         Self::path_sum_iii_new_travel(root.as_ref(), sum)
     }
+    fn path_sum_iii_new_travel(root: Option<&Rc<RefCell<TreeNode>>>, sum: i32) -> i32 {
+        let mut result = 0;
 
+        root.map(|node| {
+            if node.borrow().val == sum {
+                result += 1;
+            }
 
+            node.borrow().left.as_ref().map(|node_l| {
+                result += Self::path_sum_iii_new_travel(Some(node_l), sum)
+                    + Self::path_sum_iii_old_travel(Some(node_l), node.borrow().val, sum);
+            });
+
+            node.borrow().right.as_ref().map(|node_r| {
+                result += Self::path_sum_iii_new_travel(Some(node_r), sum)
+                    + Self::path_sum_iii_old_travel(Some(node_r), node.borrow().val, sum);
+            });
+        });
+
+        result
+    }
+
+    fn path_sum_iii_old_travel(root: Option<&Rc<RefCell<TreeNode>>>, sum: i32, target: i32) -> i32 {
+        let mut result = 0;
+
+        root.map(|node| {
+            if node.borrow().val + sum == target {
+                result += 1;
+            }
+
+            node.borrow().left.as_ref().map(|node_l| {
+                result +=
+                    Self::path_sum_iii_old_travel(Some(node_l), sum + node.borrow().val, target);
+            });
+
+            node.borrow().right.as_ref().map(|node_r| {
+                result +=
+                    Self::path_sum_iii_old_travel(Some(node_r), sum + node.borrow().val, target);
+            });
+        });
+
+        result
+    }
 }
 
+/*
+执行结果：
+通过
+显示详情
+执行用时 :
+36 ms
+, 在所有 Rust 提交中击败了
+57.14%
+的用户
+内存消耗 :
+2.2 MB
+, 在所有 Rust 提交中击败了
+100.00%
+的用户
+*/
