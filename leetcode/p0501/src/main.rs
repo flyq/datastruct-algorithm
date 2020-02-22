605a8545eda12bdeaf95c394bbedc3152b0b23ce
flyq <flyq951@gmail.com>
2020-02-22 00:57:21 -0800

    add p0501

---
+++ leetcode/p0501/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("Hello, world!");
+}
+pub struct Solution{}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #{inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn find_mode(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut m = 
