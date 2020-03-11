8534acdd1f658e13d23dc9737f3d890ca19c95cb
flyq <flyq951@gmail.com>
2020-03-10 18:49:06 -0700

    fix p1013

--- leetcode/p1013/src/main.rs
+++ leetcode/p1013/src/main.rs
@@ -13,39 +13,44 @@ impl Solution {
             return false;
         }
         sum /= 3;
-        let mut sum1 = 0;
-        let mut i = 0;
-        while sum1 != sum && i < a.len() {
-            sum1 += a[i];
+        let mut i = 0; 
+        let mut sum1 = a[i];
+        while sum1 != sum && i < a.len()-1 {
             i += 1;
+            sum1 += a[i];
         }
+
+        i += 1;
         if i > a.len()-2 {
             return false;
         }
-        sum1 = 0;
-        while sum1 != sum && i < a.len() {
-            sum1 += a[i];
+
+        sum1 = a[i];
+        while sum1 != sum && i < a.len()-1 {
             i += 1;
+            sum1 += a[i];
         }
-        if i > a.len() -1 {
+
+        i += 1;
+        if i > a.len()-1 {
             return false;
         }
+
         true                
     }
 }
-
 /*
 执行结果：
 通过
 显示详情
 执行用时 :
 8 ms
-, 在所有 rust 提交中击败了
-66.67%
+, 在所有 Rust 提交中击败了
+71.43%
 的用户
 内存消耗 :
-2.6 MB
-, 在所有 rust 提交中击败了
-100.00%
+2.5 MB
+, 在所有 Rust 提交中击败了
+75.00%
 的用户
 */
