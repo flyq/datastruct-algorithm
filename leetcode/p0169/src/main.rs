39a9fbf9a0a235b26f6ac0046e7307aa6e1d1cfe
flyq <flyq951@gmail.com>
2020-03-13 08:53:36 -0700

    add new solution

--- leetcode/p0169/src/main.rs
+++ leetcode/p0169/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
 struct Solution{}
 struct Solution2{}
+struct Solution3{}
 
 use std::collections::HashMap;
 impl Solution {
@@ -81,5 +82,40 @@ impl Solution2 {
 2.3 MB
 , 在所有 rust 提交中击败了
 100.00%
+的用户
+*/
+
+
+impl Solution3 {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        let mut maxcount = 1;
+        let mut maxvalue = nums[0];
+
+        for i in 1..nums.len() {
+            if nums[i] == maxvalue {
+                maxcount += 1;
+            } else {
+                maxcount -= 1;
+                if maxcount == 0 {
+                    maxvalue = nums[i+1];
+                }
+            }
+        }
+        maxvalue
+    }
+}
+/*
+执行结果：
+通过
+显示详情
+执行用时 :
+0 ms
+, 在所有 Rust 提交中击败了
+100.00%
+的用户
+内存消耗 :
+2.4 MB
+, 在所有 Rust 提交中击败了
+33.33%
 的用户
 */
