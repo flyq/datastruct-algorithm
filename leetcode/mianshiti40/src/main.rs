0d8d5e95219faff8c558cf3c2bd0cf20abd40e5b
flyq <flyq951@gmail.com>
2020-03-22 17:30:06 -0700

    add another solution in mianshi40

--- leetcode/mianshiti40/src/main.rs
+++ leetcode/mianshiti40/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
 }
 
 pub struct Solution {}
+pub struct Solution1 {}
 
 use std::collections::BinaryHeap;
 impl Solution {
@@ -60,3 +61,39 @@ impl Solution {
 的用户
 */
     
+
+
+impl Solution1 {
+    pub fn get_least_numbers(arr: Vec<i32>, k: i32) -> Vec<i32> {
+        if k == 0 {
+            return vec![];
+        }
+        let k = k as usize;
+        let mut heap = BinaryHeap::new();
+
+        for i in 0..arr.len() {
+            heap.push(arr[i]);
+            if heap.len()> k {
+                heap.pop();
+            }
+        }
+        heap.into_vec()
+    }
+}
+
+
+/*
+执行结果：
+通过
+显示详情
+执行用时 :
+8 ms
+, 在所有 Rust 提交中击败了
+100.00%
+的用户
+内存消耗 :
+2.1 MB
+, 在所有 Rust 提交中击败了
+100.00%
+的用户
+*/
